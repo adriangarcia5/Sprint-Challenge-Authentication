37d64a29aaf93b309549b065aa0fc52eba712934
Adrian-Garcia <agarc5@hotmail.com>
2019-10-18 11:16:34 -0500

    register & login functional for /auth

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -4,5 +4,7 @@
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  req.session && req.session.user
+    ? next()
+    : res.status(401).json({ message: "You shall not pass" });
 };
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,42 @@
-const knex = require('knex');
+const knex = require("knex");
+const configOptions = require("../knexfile").development;
+const db = knex(configOptions);
 
-const knexConfig = require('../knexfile.js');
+module.exports = {
+  find,
+  findBy,
+  findSecret,
+  register,
+  login,
+  update
+};
 
-module.exports = knex(knexConfig.development);
+function find() {
+  console.log("Got to the find function");
+  return db("users");
+}
+
+function findSecret() {
+  return db("restricted");
+}
+
+function findBy(str) {
+  console.log("str", str.toString());
+  return db("users").where({ username: str.toString() });
+}
+
+function register(resource) {
+  return db("users").insert(resource);
+}
+
+function login(resource) {
+  return db("users")
+    .insert(resource)
+    .then(ids => ({ id: ids[0] }));
+}
+
+function update(id, resource) {
+  return db("users")
+    .where("id", Number(id))
+    .update(resource);
+}
